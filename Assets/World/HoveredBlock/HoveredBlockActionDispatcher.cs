5532db0349cf259f1d10cdde8a68648d47a7ddcd
Oleg Mikhailov <oleegarch.com@gmail.com>
2025-08-10 16:48:32 +0400

    Renaming fields

--- Assets/World/HoveredBlock/HoveredBlockActionDispatcher.cs
+++ Assets/World/HoveredBlock/HoveredBlockActionDispatcher.cs
@@ -8,21 +8,21 @@ namespace World.HoveredBlock
         [SerializeField] private HoveredBlockOutlineRenderer _outliner;
         [SerializeField] private HoveredBlockObserver _observer;
         [SerializeField] private HoveredBlockBreaker _breaker;
-        [SerializeField] private HoveredBlockPicker _selector;
-        [SerializeField] private HoveredBlockPlacer _setting;
+        [SerializeField] private HoveredBlockPicker _picker;
+        [SerializeField] private HoveredBlockPlacer _placer;
         [SerializeField] private ChunksManager _chunksManager;
 
         private void OnEnable()
         {
-            _setting.OnBlockSetAttempt += HandleBlockSet;
+            _placer.OnBlockSetAttempt += HandleBlockSet;
             _breaker.OnBlockBreakAttempt += HandleBlockBroken;
         }
         private void OnDisable()
         {
-            _setting.OnBlockSetAttempt -= HandleBlockSet;
+            _placer.OnBlockSetAttempt -= HandleBlockSet;
             _breaker.OnBlockBreakAttempt -= HandleBlockBroken;
         }
         private void HandleBlockBroken(WorldPosition wc) => _chunksManager.Blocks.BreakVisible(wc);
-        private void HandleBlockSet(WorldPosition wc) => _chunksManager.Blocks.Set(wc, _selector.SelectedBlock, _selector.SelectedLayer);
+        private void HandleBlockSet(WorldPosition wc) => _chunksManager.Blocks.Set(wc, _picker.SelectedBlock, _picker.SelectedLayer);
     }
 }
