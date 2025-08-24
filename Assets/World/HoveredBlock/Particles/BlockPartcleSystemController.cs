47424273abee9a5738b7723d563d73236fb9e13a
Oleg Mikhailov <oleegarch.com@gmail.com>
2025-08-24 17:17:37 +0400

    Using MaterialPropertyBlock in BlockPartcleSystemController

--- Assets/World/HoveredBlock/Particles/BlockPartcleSystemController.cs
+++ Assets/World/HoveredBlock/Particles/BlockPartcleSystemController.cs
@@ -5,14 +5,15 @@ namespace World.HoveredBlock.Particles
 {
     public class BlockParticleSystemController : MonoBehaviour
     {
-        [SerializeField] private Material _instanceMaterial;
         [SerializeField] private ParticleSystem _breakingParticles;
 
         private ParticleSystemRenderer _breakingRenderer;
+        private MaterialPropertyBlock _mpb;
 
         private void Awake()
         {
             _breakingRenderer = _breakingParticles.GetComponent<ParticleSystemRenderer>();
+            _mpb = new MaterialPropertyBlock();
         }
         private void Start()
         {
@@ -24,17 +25,19 @@ namespace World.HoveredBlock.Particles
             transform.position = position.ToVector3Int();
 
             Texture2D texture = block.ParticleTexture;
-
             if (texture == null)
             {
                 Debug.LogWarning($"BlockParticleSystemController: no particle texture or sprite texture available for block '{block.Name}'");
                 return;
             }
 
-            _instanceMaterial.mainTexture = texture;
-            _breakingRenderer.material = _instanceMaterial;
+            _breakingRenderer.GetPropertyBlock(_mpb);
+            _mpb.SetTexture("_MainTex", texture);
+            _breakingRenderer.SetPropertyBlock(_mpb);
+
             _breakingParticles.Play();
         }
+
         public void StopBlockBreaking()
         {
             _breakingParticles.Stop();
