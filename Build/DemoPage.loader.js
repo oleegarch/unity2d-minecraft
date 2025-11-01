01cd3b116abcb1e4d262f45991ea30f2edb1bcfb
Oleg Mikhailov <oleegarch.com@gmail.com>
2025-11-01 15:21:33 +0400

    Testing dev build for debug

--- Build/DemoPage.loader.js
+++ Build/DemoPage.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 72496614,
+    wasmFileSize: 72496886,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
